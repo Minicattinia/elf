8c4c7d7911106663fb0004b7df66308cd13572c7
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2020-10-14 16:10:03 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$data = "screen_densityDpi=320&sdk=22&sign=%2BhXsbym51m6EWlipRNGU8E8%2FoAQ%3D&source=android_app&screen_density=2.0&client_user_id=179926&os=5.1.1&screen_height=900&device_id_type=3&yd_network_status=9&device_id=9af53bdd9f1d11d3&xyy=a23c01180ad911eb8afa0242ac14001a&package=ElfinTycoon&local_date=2020-10-10&language=en&screen_width=1600&phone_type=samsung_sm-g973n_5.1.1&channel=channel_google&ver=1.0.0.7.1&user_id=179926&android_id=9af53bdd9f1d11d3";
+
+
+$data2 = "screen_densityDpi=320&sdk=22&sign=%2BhXsbym51m6EWlipRNGU8E8%2FoAQ%3D&source=android_app&screen_density=2.0&client_user_id=179926&os=5.1.1&screen_height=900&device_id_type=3&yd_network_status=9&device_id=9af53bdd9f1d11d3&xyy=a23c01180ad911eb8afa0242ac14001a&package=ElfinTycoon&local_date=2020-10-10&language=en&screen_width=1600&phone_type=samsung_sm-g973n_5.1.1&channel=channel_google&ver=1.0.0.7.1&user_id=179926&android_id=9af53bdd9f1d11d3";
